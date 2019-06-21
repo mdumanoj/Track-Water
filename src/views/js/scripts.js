fe03a13767a10ec4d2221b8db01f5b8555052beb
Manoj <tsmanoj.kumar@livanova.com>
2019-06-21 17:56:19 +0530

    Designed add can page

--- src/views/js/scripts.js
+++ src/views/js/scripts.js
@@ -1,7 +1,17 @@
-(function() {
-    // Navbar Tabs
-    var navbarTab = document.getElementById('navbar-tab');
-    var instance = M.Tabs.init(navbarTab);
-    instance.select('list');
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        // Navbar Tabs
+        var navbarTab = document.getElementById('navbar-tab');
+        var tabInstance = M.Tabs.init(navbarTab);
+        tabInstance.select('list');
+        
+        // Select Option
+        var selectElement = document.querySelectorAll('select');
+        var selectInstance = M.FormSelect.init(selectElement);
 
+        // Datepicker
+        var datePickerOptions = { defaultDate : new Date(), setDefaultDate : true };
+        var datePicker = document.querySelectorAll('.datepicker');
+        var datePickerInstance = M.Datepicker.init(datePicker,datePickerOptions);
+    });
 })();
