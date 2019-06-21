35f8986bb5e8dd28479fc69d99db71f2055540b9
Manoj <tsmanoj.kumar@livanova.com>
2019-06-21 22:57:57 +0530

    Designed List Page

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port : '9000',
+    port : '8080',
     cloudantUrl : 'https://a035a1b0-fb2c-4f92-aa67-1a0db754fb74-bluemix:5b1db80516e3df826b51dcd6d592c96dc3dcc13ccacd082bf4096447f1ee470b@a035a1b0-fb2c-4f92-aa67-1a0db754fb74-bluemix.cloudantnosqldb.appdomain.cloud',
     dbName : 'trackwater'
 };
