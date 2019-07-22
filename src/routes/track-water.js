37e849ecf6e53ffe980e02e9892243ee75ae91ca
Manoj <tsmanoj.kumar@livanova.com>
2019-07-22 16:33:59 +0530

    Added summary API

--- src/routes/track-water.js
+++ src/routes/track-water.js
@@ -4,6 +4,9 @@ const db = require('../db/db');
 
 const router = express.Router();
 
+/**
+ * Add can count
+ */
 router.post('/', (req, res) => {
     db.insert(req.body)
         .then(resp => {
@@ -30,6 +33,58 @@ router.get('/month/:month', (req, res) => {
                 code: 'TW_001',
                 message: 'success',
                 data: result.docs
+            }
+            res.status(200).json(responseObj);
+        }).catch(err => {
+            console.error(err);
+            let responseObj = {
+                code: 'TW_101',
+                message: 'Application Error'
+            }
+            res.status(500).json(responseObj);
+        });
+});
+
+/**
+ * Summary of can count by person
+ */
+router.get('/summary/month/:month', (req, res) => {
+    db.find({ selector: { month: +req.params.month } })
+        .then(result => {
+            let countMap = new Map([
+                ['Ashok',0],
+                ['Barath',0],
+                ['Manoj',0],
+                ['Ranjith',0],
+                ['Kaushik',0],
+                ['Soma',0],
+                ['Velayutham',0],
+                ['Water Supplier',0]
+            ]);
+
+            // Group by user with count of can
+            result.docs.forEach((data => {
+                countMap.set(data.by,countMap.get(data.by) + data.count);
+            }));
+
+            // Convert Map into Object
+            let countObj = [];
+            countMap.forEach((val,key) => {
+                countObj.push({
+                    'name' : key,
+                    'count' : val
+                });
+            });
+
+            // Sort Object
+            countObj.sort((a,b) => {
+                return b.count - a.count;
+            });
+
+            let responseObj = {
+                code: 'TW_001',
+                message: 'success',
+                data: countObj
             }
             res.status(200).json(responseObj);
         }).catch(err => {
